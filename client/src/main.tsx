15cd6d68b85736779c53e3d999fff24db5314721
webert-abreu <escalademartech@gmail.com>
2026-01-29 02:50:14 -0300

    Final: Add Toaster e Fix Login

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner"; // <--- 1. ADICIONE ISSO AQUI
 
 // Importando as páginas
 import App from "./App";
@@ -12,15 +13,18 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    {/* 2. ADICIONE ISSO AQUI PARA OS AVISOS APARECEREM */}
+    <Toaster position="top-center" richColors />
+
     <BrowserRouter>
       <Routes>
-        {/* ROTA 1: A Vitrine (Pública) - É aqui que o usuário cai ao digitar o site */}
+        {/* ROTA 1: A Vitrine (Pública) */}
         <Route path="/" element={<Landing />} />
 
         {/* ROTA 2: O Login (Entrada) */}
         <Route path="/login" element={<Login />} />
 
-        {/* ROTA 3: O Sistema (Privado) - Onde acontece o download */}
+        {/* ROTA 3: O Sistema (Privado) */}
         <Route path="/app" element={<App />} />
       </Routes>
     </BrowserRouter>
